f0476d7491c033779de86c210feb7a7a9fb0b824
yusok7 <diddbtjr7@naver.com>
2021-12-20 18:22:32 +0900

    HelloResponseDto 작성

---
+++ src/main/java/com/yusok7/springawswebservice/web/dto/HelloResponseDto.java
@@ -0,0 +1,13 @@
+package com.yusok7.springawswebservice.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+
+}
