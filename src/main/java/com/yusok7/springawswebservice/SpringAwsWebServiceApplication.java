7eb308181ad8d3d22585525c4e85b79df1e5c910
yusok7 <diddbtjr7@naver.com>
2021-12-20 17:44:18 +0900

    프로젝트 생성

---
+++ src/main/java/com/yusok7/springawswebservice/SpringAwsWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yusok7.springawswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAwsWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yusok7/springawswebservice/SpringAwsWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusok7.springawswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAwsWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
