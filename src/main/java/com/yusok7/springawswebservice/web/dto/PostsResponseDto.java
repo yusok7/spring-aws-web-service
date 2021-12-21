7c1543a086efc3dbe3d71faea49c6c3481f669c0
yusok7 <diddbtjr7@naver.com>
2021-12-21 17:11:58 +0900

    Posts 응답 dto

---
+++ src/main/java/com/yusok7/springawswebservice/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.yusok7.springawswebservice.web.dto;
+
+import com.yusok7.springawswebservice.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
