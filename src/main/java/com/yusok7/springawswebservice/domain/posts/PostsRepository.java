f28a766ca2ef2bd323a581c6c25ad3a6ff932f98
yusok7 <diddbtjr7@naver.com>
2021-12-25 17:42:50 +0900

    PostsRepository 기능 추가

--- src/main/java/com/yusok7/springawswebservice/domain/posts/PostsRepository.java
+++ src/main/java/com/yusok7/springawswebservice/domain/posts/PostsRepository.java
@@ -1,9 +1,15 @@
 package com.yusok7.springawswebservice.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /**
  * Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다. 즉 하나의 도메인 패키지에서 함께 관리한다.
  */
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
