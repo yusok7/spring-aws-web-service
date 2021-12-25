f73d5508945f6fbe1ccaeddb1da68956567996f1
yusok7 <diddbtjr7@naver.com>
2021-12-25 17:44:01 +0900

    PostsService 기능 추가

--- src/main/java/com/yusok7/springawswebservice/service/posts/PostsService.java
+++ src/main/java/com/yusok7/springawswebservice/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.yusok7.springawswebservice.service.posts;
 
 import com.yusok7.springawswebservice.domain.posts.Posts;
 import com.yusok7.springawswebservice.domain.posts.PostsRepository;
+import com.yusok7.springawswebservice.web.dto.PostsListResponseDto;
 import com.yusok7.springawswebservice.web.dto.PostsResponseDto;
 import com.yusok7.springawswebservice.web.dto.PostsSaveRequestDto;
 import com.yusok7.springawswebservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,19 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        postsRepository.delete(posts);
     }
 }
