c55cf4bb65d880fcd6b835d9ae3053651a77e6df
yusok7 <diddbtjr7@naver.com>
2021-12-25 17:35:36 +0900

    JPA Auditing 기능

--- src/main/java/com/yusok7/springawswebservice/SpringAwsWebServiceApplication.java
+++ src/main/java/com/yusok7/springawswebservice/SpringAwsWebServiceApplication.java
@@ -2,12 +2,15 @@ package com.yusok7.springawswebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * 앞으로 만들 프로젝트의 메인 클래스
  * @SpringBootApplication으로 인해 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정된다.
  * @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에 이 클래스는 항상 프로젝트의 최상단에 위치해야만 한다.
  */
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringAwsWebServiceApplication {
 
