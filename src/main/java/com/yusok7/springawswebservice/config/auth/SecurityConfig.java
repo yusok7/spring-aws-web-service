1ba2722cc351d3ae5bce9eee1f4c52fd51434b3d
yusok7 <diddbtjr7@naver.com>
2021-12-27 16:39:50 +0900

    SecurityConfig 작성

---
+++ src/main/java/com/yusok7/springawswebservice/config/auth/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.yusok7.springawswebservice.config.auth;
+
+import com.yusok7.springawswebservice.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * OAuth 라이브러리를 이용한 소셜 로그인 설정 코드
+ */
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
