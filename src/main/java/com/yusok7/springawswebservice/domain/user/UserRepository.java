a28ba8cf81b92dc1a5364c3c7b65c5a5f9a3e579
yusok7 <diddbtjr7@naver.com>
2021-12-27 16:35:04 +0900

    User Repository 작성

---
+++ src/main/java/com/yusok7/springawswebservice/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yusok7.springawswebservice.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}
