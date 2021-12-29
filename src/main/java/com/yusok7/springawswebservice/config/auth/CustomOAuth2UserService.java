8711309473c888613af116d5e3bcdcb705bfd0c5
yusok7 <diddbtjr7@naver.com>
2021-12-29 15:37:08 +0900

    OAuth2UserService 주석추가 (공부용도)

--- src/main/java/com/yusok7/springawswebservice/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/yusok7/springawswebservice/config/auth/CustomOAuth2UserService.java
@@ -30,16 +30,28 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
         OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
         OAuth2User oAuth2User = delegate.loadUser(userRequest);
 
+
+        // 현재 로그인 진행 중인 서비스를 구분하는 코드
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        // OAuth2 로그인 진행 시 키가 되는 필드값을 이야기한다. Primary Key와 같은 의미이다.
         String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName();
 
+        // OAuth2UserService를 통해 가져온 OAuth2User의 attribute를 담을 클래스다.
         OAuthAttributes attributes = OAuthAttributes.of(registrationId, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
 
+        /**
+         * SessionUser
+         * - 세션에 사용자 정보를 저장하기 위한 Dto 클래스
+         * - 왜 User클래스를 쓰지 않고 새로 만들어서 써야만 할까?
+         * - User 클래스에 직렬화를 구현하면 너무 많은 부수적인 부분을 생각해야한다.
+         * - 책 188p
+         */
         httpSession.setAttribute("user", new SessionUser(user));
 
         return new DefaultOAuth2User(
